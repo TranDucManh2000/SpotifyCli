8df433180528726e17faa485a67e7d71d4d831dd
Tran Manh <84903259+TranDucManh2000@users.noreply.github.com>
2022-05-26 17:04:39 +0700

    updateeslin

---
+++ compoment/body/Indexs.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const Indexs = () => {
+  return (
+    <View>
+      <Text>adsđs</Text>
+    </View>
+  );
+};
+
+export default Indexs;
